c2b22dfefbac7926f98eabeb334906c13ad23cc3
Aleia Knight <aleiaknight@gmail.com>
2019-11-14 10:42:53 -0800

    basic functions for questions setup and working

--- quiz.py
+++ quiz.py
@@ -21,25 +21,62 @@ Q4 = "How many Earth's would fit insode Jupiter?"
 # True/False
 Q5 = """Apollo 10's command module was called "Charlie Brown" and the lunar module was called "Snoopy."  """
 # True
+# if answer = False, respond -> 
 
 Q6 = ''' Bessie Coleman, known as "Queen Bess, Daredevil Aviator," was the first African-American woman aviator.
 She received her pilot's certificate in 1921 in America and learned stunt-flying there. '''
 # False, she got her license in France
 
+#Colors for use later
+class colors:
+    purple = '\033[35m'
+    grey = '\033[37m'
+    red='\033[31m'
+
 
 
 # FUNCTIONS
-def multiple_choice(question, answer):
-    print(question)
-    user_answer = input("Input a answer: \n").lower()
+def multiple_choice_question(question, answer):
+    print(colors.grey, question)
+    user_answer = input("Input a choice (a-e): \n").lower()
     if user_answer == answer:
         print("You are correct!")
+        return 1
     else:
-        print("You are incorrect")
+        print(colors.red, "You are incorrect")
+        return 0
 
+def numerical_question(question, answer):
+    print(colors.grey, question)
+    user_answer = input("Input a number: \n")
+    if user_answer == answer and user_answer.isdigit():
+        print("You are correct!")
+        return 1
+    # elif user_answer == answer and user_answer.isdigit == False:
+    #     print("Inproper format")
+    else:
+        print(colors.red, "You are incorrect")
+        return 0
+
+def true_false_question(question, answer, fact):
+    print(colors.grey, question)
+    user_answer = input("Enter True(t) or False(f): \n").lower()
+    if user_answer == answer:
+        print("You are correct!")
+        return 1
+    else:
+        print(colors.red, "You are incorrect/n", fact)
+        print()
+        return 0
 
 
 
 # TESTS
-multiple_choice(Q1, 'e')
-multiple_choice(Q2, 'b')
+multiple_choice_question(Q1, 'e')
+multiple_choice_question(Q2, 'b')
+
+numerical_question(Q3, '1')
+numerical_question(Q4, '1000')
+
+true_false_question(Q5, 't', 'Snoopy, the Peanuts Comic Strip character is the astronauts personal safety mascot')
+true_false_question(Q6, 'f', 'She got her license in France')
