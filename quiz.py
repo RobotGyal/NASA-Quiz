075b68d9dbc17f3d4fd30d441fa040a02bc72cc1
Aleia Knight <aleiaknight@gmail.com>
2019-11-19 18:42:24 -0800

    added menu for replay

--- quiz.py
+++ quiz.py
@@ -1,24 +1,23 @@
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
-QUIZ= ["What is the literal definition of Equinox? \n a) Equal Dark  \n b) Year Night  \n c) Day Night  \n d) Day Dark  \n e) Equal Night\n",
+QUIZ= ["\nWhat is the literal definition of Equinox? \n a) Equal Dark  \n b) Year Night  \n c) Day Night  \n d) Day Dark  \n e) Equal Night\n",
 
-"""Before NASA was formed, the National Advisory Committee for Aeronautics (NACA) was started
+"""\nBefore NASA was formed, the National Advisory Committee for Aeronautics (NACA) was started
 to supervise and direct the scientific study of the problems of flight.
 The NACA also directed and conducted research and experiments in aeronautics.
 Which President found NACA?
 \n a) Franklin D. Roosevelt  \n b) Woodrow Wilson  \n c) Ronald Reagan  \n d) Theodore Roosevelt  \n e) John F. Kennedy\n""",
 
-"NASA became operational how many years after the Soviets launched Sputnik 1, the world's first artificial satellite?",
+"\nNASA became operational how many years after the Soviets launched Sputnik 1, the world's first artificial satellite?",
 
-"How many Earth's would fit insode Jupiter?",
+"\nHow many Earth's would fit insode Jupiter?",
 
-"""Apollo 10's command module was called "Charlie Brown" and the lunar module was called "Snoopy."  """,
+"""\nApollo 10's command module was called "Charlie Brown" and the lunar module was called "Snoopy."  """,
 
-''' Bessie Coleman, known as "Queen Bess, Daredevil Aviator," was the first African-American woman aviator.
+'''\nBessie Coleman, known as "Queen Bess, Daredevil Aviator," was the first African-American woman aviator.
 She received her pilot's certificate in 1921 in America and learned stunt-flying there. '''
 
 ]
-
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
 
 
@@ -83,24 +82,32 @@ def true_false_question(question, answer, fact):
 # true_false_question(Q5, 't', 'Snoopy, the Peanuts Comic Strip character is the astronauts personal safety mascot')
 # true_false_question(Q6, 'f', 'She got her license in France\n')
 
-# print("\nCorrect: ", len(correct_questions))
-# print("Incorrect: ", len(incorrect_questions))
-
-
-# questions = {
-#     'Q1':Q1,
-#     'Q2':Q2,
-#     'Q3':Q3,
-#     'Q4':Q4,
-#     'Q5':Q5,
-#     'Q6':Q6,
-# }
-
-# for i in questions:
-#     print(i, '\n')
-# print(questions['Q2'])
-
 # print(QUIZ[0])
 
-for count, ele in enumerate(QUIZ,1):
-    print(count, ele, '\n')
+running = True
+while running:
+
+    # for count, ele in enumerate(QUIZ,1):
+    #     print(count, ele, '\n')
+    print(colors.purple, "\n\nWelcome to the NASA QUIZ!!!! Prepare to put your knowledge to the test!!\n")
+
+    print(colors.purple, "\nThe first 2 questions will be multiple choice. Please enter a letter(a-e)\n")
+    multiple_choice_question(QUIZ[0], 'e')
+    multiple_choice_question(QUIZ[1], 'b')
+
+    print(colors.purple, "\nThe next 2 questions will be numerical. Please enter a whole number\n")
+    numerical_question(QUIZ[2], '1')
+    numerical_question(QUIZ[3], '1000')
+
+    print(colors.purple, "\nThe last 2 questions will be true/false. Please enter True(t) or False(f) \n")
+    true_false_question(QUIZ[4], 't', 'Snoopy, the Peanuts Comic Strip character is the astronauts personal safety mascot')
+    true_false_question(QUIZ[5], 'f', 'She got her license in France\n')
+    
+    print(colors.grey, "\nCorrect: ", len(correct_questions))
+    print("Incorrect: ", len(incorrect_questions))
+
+    play_again = input("Would you like to play again? Press Yes(y) or No(n)\n").lower()
+    if play_again == 'y':
+        continue
+    else:
+        running = False
